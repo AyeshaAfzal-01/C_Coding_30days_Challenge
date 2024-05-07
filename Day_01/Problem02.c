757904729b25ec48de4e06ad3ebce5f548c7486b
Ayesha Afzal <154336255+AyeshaAfzal-01@users.noreply.github.com>
2024-05-07 08:10:08 +0500

    Format Problem2

--- Day_01/Problem02.c
+++ Day_01/Problem02.c
@@ -1,17 +1,20 @@
 /*
+
 Problem 2:
 Write a program that shows the following output on the display.
 Sample Run:
 This is Coding in “C language” 30 days challenge.
 This course is organized by C_Coding_30days_Challenge.
-*/ 
 
-#include<stdio.h>
+*/
 
-int main() {
+#include <stdio.h>
+
+int main()
+{
 
     printf("This is Coding in \"C Language\" 30 days challenge.\n");
     printf("This Challenge is organized by C_Coding_30days_Challenge.");
-    
+
     return 0;
 }
