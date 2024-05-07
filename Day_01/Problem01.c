dab80f12a34fcb35709eb02505de9b6f860ff320
Ayesha Afzal <154336255+AyeshaAfzal-01@users.noreply.github.com>
2024-05-07 08:08:57 +0500

    Format Problem1

--- Day_01/Problem01.c
+++ Day_01/Problem01.c
@@ -1,13 +1,18 @@
-/* Problem 1:
+/*
+
+ Problem 1:
 Write a program that shows the following output on the display.
 Sample Run:
-My name is [Your Name].*/
+My name is [Your Name].
 
-#include<stdio.h>
+*/
 
-int main() {
+#include <stdio.h>
+
+int main()
+{
 
     printf("My name is Ayesha.");
-    
+
     return 0;
 }
